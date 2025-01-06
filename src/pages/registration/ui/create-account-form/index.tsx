d83948323d653dacb5842a4f479ee5b67a8158e3
Evgeniy Blank <blank.eugeny@yandex.ru>
2025-01-06 12:14:00 +0200

    feature/add-registration-page

---
+++ src/pages/registration/ui/create-account-form/index.tsx
@@ -0,0 +1,49 @@
+import { useCreateAccount } from "@/features/auth";
+import { authErrorCheck } from "@/shared/lib/auth-error-check";
+import Button from "@/shared/ui/button";
+import Input from "@/shared/ui/input";
+import { useState } from "react";
+
+const CreateAccountForm = () => {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const { handleCreate, error, isLoading } = useCreateAccount();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleCreate({ email, password });
+  };
+
+  return (
+    <form className="flex flex-col gap-y-4" onSubmit={handleSubmit}>
+      <Input
+        type="email"
+        name="email"
+        id="email"
+        label="Почта"
+        placeholder="Введите почту"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <Input
+        type="password"
+        name="password"
+        id="password"
+        label="Пароль"
+        placeholder="Введите пароль"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      {!!error && <p className="text-red-200">{authErrorCheck(error)}</p>}
+
+      <Button isLoading={isLoading} className="ml-auto mt-4" type="submit">
+        Зарегистрироваться
+      </Button>
+    </form>
+  );
+};
+
+export default CreateAccountForm;
---
+++ src/pages/registration/ui/registration-page.tsx
@@ -0,0 +1,28 @@
+import { RoutePath } from "@/app/providers/router";
+import { GoogleAuthButton } from "@/features/auth";
+import Container from "@/shared/ui/container";
+import { Heading } from "@/shared/ui/heading";
+import { AppLink } from "@/shared/ui/link";
+import CreateAccountForm from "./create-account-form";
+
+export const RegistrationPage = () => {
+  return (
+    <main className="min-h-[calc(100vh-var(--header))] bg-rose-950 flex items-center">
+      <Container className="relative bottom-10">
+        <Heading className="text-center">Регистрация</Heading>
+        <div className="bg-rose-700 border-rose-900 shadow-md rounded-lg mx-auto mt-8 px-4 py-8 max-w-[480px] flex flex-col gap-y-4">
+          <GoogleAuthButton />
+
+          <p className="text-center text-rose-100">
+            или введите данные для регистрации
+          </p>
+
+          <CreateAccountForm />
+        </div>
+        <div className="text-center mt-2">
+          Есть аккаунт? - <AppLink to={RoutePath.LOGIN}>Войти</AppLink>
+        </div>
+      </Container>
+    </main>
+  );
+};
