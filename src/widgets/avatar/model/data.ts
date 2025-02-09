9a290d56ba86e0e584981d7380591a7c433a3791
Blank Evgeniy <blank.eugeny@yandex.ru>
2025-02-10 00:53:51 +0300

    update/avatars

--- src/widgets/avatar/model/data.ts
+++ src/widgets/avatar/model/data.ts
@@ -4,5 +4,24 @@ import avatar3 from "@/shared/assets/images/avatars/av3.jpg";
 import avatar4 from "@/shared/assets/images/avatars/av4.jpg";
 import avatar5 from "@/shared/assets/images/avatars/av5.jpg";
 import avatar6 from "@/shared/assets/images/avatars/av6.jpg";
+import avatar7 from "@/shared/assets/images/avatars/av7.jpg";
+import avatar8 from "@/shared/assets/images/avatars/av8.jpg";
+import avatar9 from "@/shared/assets/images/avatars/av9.jpg";
+import avatar10 from "@/shared/assets/images/avatars/av10.jpg";
+import avatar11 from "@/shared/assets/images/avatars/av11.jpg";
+import avatar12 from "@/shared/assets/images/avatars/av12.jpg";
 
-export const avatars = [avatar1, avatar2, avatar3, avatar4, avatar5, avatar6];
+export const avatars = [
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  avatar5,
+  avatar6,
+  avatar7,
+  avatar8,
+  avatar9,
+  avatar10,
+  avatar11,
+  avatar12,
+];
